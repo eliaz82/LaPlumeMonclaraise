2fc9fdc63a4da8a63ca1917dd3899b0a9adae611
eliaz <eliaz824582@gmail.com>
2025-01-14 16:12:23 +0100

    affe

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -6,24 +6,128 @@
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?= base_url('css/main.css') ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <style>
 </style>
-<nav>
-  <ul>
-    <li><a href="#">Accueil</a></li>
-    <li>
-      <a href="#">L'Association</a>
-      <ul>
-        <li><a href="#">L'Équipe</a></li>
-        <li><a href="#">L'Histoire</a></li>
-        <li><a href="#">Partenaires</a></li>
-      </ul>
-    </li>
-    <li><a href="#">Calendrier</a></li>
-    <li><a href="#">Inscription</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="image/logo.jpg" alt="logo" class="img-fluid">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Accueil</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        L'Association
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">L'Équipe</a></li>
+                        <li><a class="dropdown-item" href="#">L'Histoire</a></li>
+                        <li><a class="dropdown-item" href="#">Partenaires</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Fait marquant</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Calendrier</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Evenement</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inscription</a>
+                </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Contact</a> </li>
+            </ul>
+        </div>
+    </div>
 </nav>
+
+<body>
+    <div class="container pt-5">
+        <h1>ouiouioui</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+
+
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+
+
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+
+
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
+
+        <h1>Hello, world!</h1>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 <?= $this->renderSection('contenu') ?>
