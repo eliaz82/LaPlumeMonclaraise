971a25d6e2967055f4e830cdcc721aa278e6dd40
emmanuel <emmanuel.basck@gmail.com>
2025-01-27 21:44:32 +0100

    dit moi ce que t'en pense sac a merde

--- app/Views/contact.php
+++ app/Views/contact.php
@@ -3,14 +3,9 @@
 
 
 <div class="container">
-    <h2 class="text-center mb-4">Formulaire de Contact</h2>
 
     <!-- Contact update -->
     <!-- Bouton pour ouvrir le modal -->
-    <button id="bouton-modifier-contact" class="btn btn-primary mb-3" data-bs-toggle="modal"
-        data-bs-target="#modalModifierContact">
-        <i class="fa fa-edit" aria-hidden="true"></i> Modifier e-mail de contact
-    </button>
 
 
     <!-- Modal de modification de l'e-mail de contact -->
@@ -44,51 +39,59 @@
 
 
     <!-- Contact Form -->
-    <form action="<?= route_to('contactSubmit'); ?>" method="post"
-        class="p-4 border border-light rounded shadow-lg bg-white">
-        <div class="mb-3">
-            <label for="name" class="form-label">Nom:</label>
-            <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom" required>
-        </div>
+    <div class="contact-container">
 
-        <div class="mb-3">
-            <label for="phone" class="form-label">Téléphone:</label>
-            <input type="text" id="phone" name="phone" class="form-control" placeholder="Votre téléphone" required>
-        </div>
+        <form class="contact-form" action="<?= route_to('contactSubmit'); ?>" method="post">
+            <p id="contact-heading">Contactez-nous</p>
 
-        <div class="mb-3">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" id="email" name="email" class="form-control" placeholder="Votre email" required>
-        </div>
-
-        <div class="mb-3">
-            <label for="subject" class="form-label">Objet:</label>
-            <input type="text" id="subject" name="subject" class="form-control" placeholder="Objet de votre message"
-                required>
-        </div>
-
-        <div class="mb-3">
-            <label for="message" class="form-label">Message:</label>
-            <textarea id="message" name="message" rows="5" class="form-control" placeholder="Votre message"
-                required></textarea>
-        </div>
-
-        <!-- boutton contact envoyer -->
-        <button class="btn-unique">
-            <div class="icon-wrapper-outer">
-                <div class="icon-wrapper-inner">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                        <path fill="none" d="M0 0h24v24H0z"></path>
-                        <path fill="currentColor"
-                            d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z">
-                        </path>
-                    </svg>
-                </div>
+            <div class="contact-field">
+                <input autocomplete="off" placeholder="Nom" id="name" name="name" class="contact-input" type="text"
+                    required>
             </div>
-            <span class="btn-label">Envoyer</span>
-        </button>
 
-    </form>
+            <div class="contact-field">
+                <input autocomplete="off" placeholder="Téléphone" id="phone" name="phone" class="contact-input"
+                    type="text" required>
+            </div>
+
+            <div class="contact-field">
+                <input autocomplete="off" placeholder="Email" id="email" name="email" class="contact-input" type="email"
+                    required>
+            </div>
+
+            <div class="contact-field">
+                <input autocomplete="off" placeholder="Objet" id="subject" name="subject" class="contact-input"
+                    type="text" required>
+            </div>
+
+            <div class="contact-field">
+                <textarea autocomplete="off" placeholder="Message" id="message" name="message" rows="5"
+                    class="contact-textarea" required></textarea>
+            </div>
+
+            <div class="button-container">
+                <button class="btn-unique">
+                    <div class="icon-wrapper-outer">
+                        <div class="icon-wrapper-inner">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path fill="currentColor"
+                                    d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z">
+                                </path>
+                            </svg>
+                        </div>
+                    </div>
+                    <span class="btn-label">Envoyer</span>
+                </button>
+            </div>
+            <button id="bouton-modifier-contact" class="btn btn-primary mb-3" data-bs-toggle="modal"
+                data-bs-target="#modalModifierContact">
+                <i class="fa fa-edit" aria-hidden="true"></i> Modifier e-mail de contact
+            </button>
+
+
+        </form>
+    </div>
 </div>
 
 <?= $this->endSection() ?>
