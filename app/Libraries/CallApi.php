68cb7bc2ef7f98664df6d1ac3292bc92044b259f
eliaz <eliaz824582@gmail.com>
2025-01-28 19:12:46 +0100

    mr je vous jure j'ai travaillé

---
+++ app/Libraries/CallApi.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Libraries;
+
+class CallApi
+{
+    public static function callApi(string $url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            curl_close($ch);
+            return ['error' => 'Erreur cURL : ' . curl_error($ch)];
+        }
+
+        curl_close($ch);
+        return json_decode($response, true);
+    }
+}
