d357846c1f28f7dfacc5db3f6ef29aa58b3ba1e1
eliaz <eliaz824582@gmail.com>
2025-02-05 13:46:52 +0100

    gnfthnf

--- app/Controllers/Evenement.php
+++ app/Controllers/Evenement.php
@@ -16,64 +16,64 @@ class Evenement extends BaseController
     }
     public function evenement(string $id = null): string
     {
-        // Récupération des posts depuis Facebook
-        $posts = $this->facebookCache->getFacebookPosts();
-        $hashtags = $this->facebookModel->where('pageName', 'evenementCalendrier')->findAll();
+        // Récupération des posts et des hashtags
+        $posts      = $this->facebookCache->getFacebookPosts();
+        $hashtags   = $this->facebookModel->where('pageName', 'evenementCalendrier')->findAll();
         $hashtagList = array_column($hashtags, 'hashtag');
 
-        // Filtrer les posts pour ne conserver que ceux qui contiennent l'un des hashtags
+        // Filtrer les posts qui contiennent l'un des hashtags
         $filteredPosts = array_filter($posts['data'], function ($post) use ($hashtagList) {
-            if (isset($post['message'])) {
-                foreach ($hashtagList as $hashtag) {
-                    if (strpos($post['message'], $hashtag) !== false) {
-                        return true;
-                    }
+            if (!isset($post['message'])) {
+                return false;
+            }
+            foreach ($hashtagList as $hashtag) {
+                if (strpos($post['message'], $hashtag) !== false) {
+                    return true;
                 }
             }
             return false;
         });
+
+        // Date actuelle fixée à 23:59:59
         $currentDate = new \DateTime();
         $currentDate->setTime(23, 59, 59);
-        $limitDate = new \DateTime();
-        $limitDate->add(new \DateInterval('P10D')); // Ajouter 10 jours
-        // Ajouter l'image (si présente) à chaque post
+
+        // Traitement de chaque post
         foreach ($filteredPosts as &$post) {
             $post['image'] = $post['attachments']['data'][0]['media']['image']['src'] ?? null;
+            preg_match('/\*(.*?)\*/', $post['message'], $matches);
+            $post['titre'] = $matches[1] ?? substr($post['message'], 0, 50);
 
-            // Extraction du titre (entre *...*)
-            preg_match('/\*(.*?)\*/', $post['message'], $titleMatches);
-            $post['titre'] = isset($titleMatches[1]) ? $titleMatches[1] : substr($post['message'], 0, 50);
-
-            $eventDate = null;
             if (preg_match('/(\d{2}\/\d{2}\/\d{4})/', $post['message'], $matches)) {
-                $eventDate = $matches[1];
-            }
-
-            if ($eventDate) {
-                list($day, $month, $year) = explode('/', $eventDate);
+                list($day, $month, $year) = explode('/', $matches[1]);
                 if (checkdate($month, $day, $year)) {
-                    $post['date'] = $eventDate;
-
-                    // Comparer la date avec aujourd'hui
+                    $post['date'] = $matches[1];
                     $eventDateTime = new \DateTime("$year-$month-$day");
                     $eventDateTime->setTime(23, 59, 59);
-
-                    if ($eventDateTime < $currentDate) {
-                        $post['status'] = "Événement passé";
-                    } else {
-                        $post['status'] = "Événement futur";
-                    }
+                    $post['status'] = ($eventDateTime < $currentDate) ? "Événement passé" : "Événement futur";
                 }
             } else {
                 $post['status'] = "Date inconnue";
             }
         }
 
+        // Seuil pour afficher les événements futurs ou passés depuis moins d'1 jour
+        $threshold = (clone $currentDate)->sub(new \DateInterval('P1D'));
+        // Seuil pour afficher les événements futurs ou passés depuis moins d'1 mois
+        //$threshold = (clone $currentDate)->sub(new \DateInterval('P1M'));
+        $filteredPosts = array_filter($filteredPosts, function ($post) use ($threshold) {
+            if (isset($post['date'])) {
+                list($day, $month, $year) = explode('/', $post['date']);
+                $eventDateTime = new \DateTime("$year-$month-$day");
+                $eventDateTime->setTime(23, 59, 59);
+                return $eventDateTime >= $threshold;
+            }
+            return true;
+        });
 
-        // Afficher la vue de l'événement en lui passant les données de l'événement trouvé
         return view('evenements', [
             'posts'       => $filteredPosts,
-            'highlightId' => $id  // Peut être null si aucun ID n'est fourni
+            'highlightId' => $id,
         ]);
     }
 }
