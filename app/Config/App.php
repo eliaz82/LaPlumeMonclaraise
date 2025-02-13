4f173738dd61e0063c3209b2e4cb42ae80b95bd4
eliaz <eliaz824582@gmail.com>
2025-02-13 14:30:23 +0100

    efqge

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://laplumemonclaraise.local/';
+    public string $baseURL = 'https://laplumemonclaraise.fr/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
