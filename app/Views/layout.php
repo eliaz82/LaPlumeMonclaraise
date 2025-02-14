3c34f6428a304f2a83a5bb429f286a229ae21273
manu <emmanuel.basck@gmail.com>
2025-02-14 10:19:09 +0100

    titre

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -445,7 +445,7 @@
                         <p class="modal-text">Bienvenue sur notre nouveau site !</p>
                         <p class="modal-description">Découvrez notre communauté de badminton et profitez d'une expérience
                             fluide et agréable.</p>
-                        <p class="modal-cta">Rejoignez-nous maintenant ! 🏸</p>
+                            <p class="modal-cta">Explorez nos contenus et restez informé des dernières actualités ! 🏸</p>
                     </div>
                     <div class="modal-footer custom-modal-footer">
                         <button type="button" class="btn btn-primary custom-btn-primary" data-bs-dismiss="modal">Merci
