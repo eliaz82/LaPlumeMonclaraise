bb74649788e7610dc699131862ddc87c67d2dbe4
eliaz <eliaz824582@gmail.com>
2025-02-14 11:41:56 +0100

    kugk

--- public/cron_check_token.php
+++ public/cron_check_token.php
@@ -1,12 +1,11 @@
 <?php
-// Inclure le framework CodeIgniter
-require __DIR__ . '/vendor/autoload.php'; // Ajuste ce chemin selon ta structure
+// Utilisation de CURL pour faire une requête HTTP à la route définie
+$url = 'https://laplumemonclaraise.fr/check-token-email'; // Remplace par l'URL correcte de ta route
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30); // 30 secondes pour attendre la réponse
+$response = curl_exec($ch);
+curl_close($ch);
 
-// Démarrer le framework CodeIgniter
-$app = require_once __DIR__ . '/app/Config/Paths.php';
-$env = \Config\Services::getEnv();
-
-// Appel à ta méthode de contrôleur directement
-$controller = new \App\Controllers\Facebook(); // Remplace 'Facebook' par ton contrôleur
-$controller->check_and_send_email();  // Appel de la méthode dans le contrôleur
 ?>
